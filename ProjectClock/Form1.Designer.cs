dcc96ec8984b83148cb555d7daef3fdfc7642a28
Maksofer-PC <maksofer@gmail.com>
2021-01-23 16:02:55 +0300

    ProjectClock

---
+++ ProjectClock/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Проект_CLOCK {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(129, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 96);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(150, 275);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(52, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "&Timer";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(248, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Start/Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(329, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "&Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 300);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.timer1_Tick);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ProjectClock/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Проект_CLOCK {
+    public partial class Form1 : Form {
+        private DateTime startTime, pauseTime;
+        private TimeSpan pauseSpan;
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            Form1_Resize: Form1_Resize(this, null); 
+            if (checkBox1.Checked) {
+                TimeSpan s = DateTime.Now - startTime - pauseSpan;
+                label1.Text = string.Format("Timer: {0}:{1}", s.Minutes * 60 + s.Seconds, s.Milliseconds / 100);
+            } 
+            else
+            {
+                label1.Text = DateTime.Now.ToLongTimeString();
+            }
+                
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) {
+                startTime = DateTime.Now;
+                pauseSpan = TimeSpan.Zero;
+                timer1.Interval = 100;
+            } 
+            else
+            {
+                timer1.Interval = 1000;
+            }                
+            timer1_Tick(this, null); 
+            button1.Enabled = button2.Enabled = checkBox1.Checked; 
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            if (timer1.Enabled)
+            {
+                pauseSpan += DateTime.Now - pauseTime;
+
+                
+            }
+            else
+            {
+                pauseTime = DateTime.Now;
+            }
+               
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Clicks == 2) checkBox1.Checked = !checkBox1.Checked;
+            else
+            {
+                if (!button1.Enabled)
+                {
+                    return;
+                }
+                if (e.Button == MouseButtons.Left)
+                {
+                    button1_Click(this, null);
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+                    button2_Click(this, null);
+                }
+            }
+                   
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Text = WindowState == FormWindowState.Minimized ?
+                label1.Text : "Clock";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            pauseTime = startTime;
+            pauseSpan = TimeSpan.Zero;
+            label1.Text = "Timer: 0:0";
+        }
+    }
+}
